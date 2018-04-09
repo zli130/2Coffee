b5a031fe5c2a5867c89c692f2fca4511ee8194d5
John Li <lizoya2011@gmail.com>
2018-04-10 09:41:45 +1200

    Update firebase.js
    config

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase'
 
 const app = initializeApp({
-  apiKey: 'AIzaSyA3Z49yGx2FuF5waiEpiQNCqFOSAyRuyvE',
-  authDomain: 'john-2c602.firebaseapp.com',
-  databaseURL: 'https://john-2c602.firebaseio.com',
-  projectId: 'john-2c602',
-  storageBucket: 'john-2c602.appspot.com',
-  messagingSenderId: '606182141935'
+  apiKey: 'FIREBASE_KEY',
+  authDomain: 'FIREBASE_DOMAIN',
+  databaseURL: 'FIREBASE_URL',
+  projectId: 'FIREBASE_PROJECT_ID',
+  storageBucket: 'FIREBASE_STORAGE,
+  messagingSenderId: 'MESSAGE_SENDER_ID'
 })
 
 export const db = app.database()
